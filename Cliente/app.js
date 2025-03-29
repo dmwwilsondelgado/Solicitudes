18674438fd5ffc99ddd6f27dc0daa90778aa85cf
✡ 𝓦𝓲𝓵𝓲 𝓓𝓮𝓵𝓰𝓪𝓭𝓸 ✡ <wilsondelgadomoreno@gmail.com>
2025-03-28 21:17:58 -0500

    avanze

--- Cliente/app.js
+++ Cliente/app.js
@@ -1,25 +1,25 @@
-// fetch("http://localhost:3000/posts").then((datos)=>{
-//     const posts = datos.json().then((posts) => {
-//         console.log(posts);
-//     })
-// })
-
-
-const guuardar = () => {
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-        method: 'POST',
-        body: JSON.stringify({
-          title: 'adso2894667',
-          author: 'Wilson Delgado', 
-          userId: 1,
-        }),
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-        },
-      })
-        .then((response) => response.json())
-        .then((json) => console.log(json));
+const url = "http://localhost:3000/";
+const solicitud = async (url) =>{
+  const respuesta = await fetch(`${url}posts`);
+  const datos = await respuesta.json();
+  return datos;
 }
-
-
-guuardar();
+solicitud(url).then(data =>
+  console.log(data)
+)
+const guardar = () => {
+  fetch('http://localhost:3000/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+      title: 'foo',
+      body: 'bar',
+      userId: 1,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
+guardar();
